85278ef3c3a3b2bd46827a9fff1453d8e721e186
mikiqqqq <107814991+mikiqqqq@users.noreply.github.com>
2022-08-03 01:11:02 +0200

    Testing Azure Database Connection 3

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "C:\Users\Filip\Desktop\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($con, "berlin-news-server.mysql.database.azure.com", "berlin_news_mng", "Admin-BZ$&3", "berlin-news-database", 3306, MYSQLI_CLIENT_SSL)
+$dbc = mysqli_init();
+mysqli_ssl_set($dbc,NULL,NULL, "C:\Users\Filip\Desktop\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($dbc, "berlin-news-server.mysql.database.azure.com", "berlin_news_mng", "Admin-BZ$&3", "berlin-news-database", 3306, MYSQLI_CLIENT_SSL)
 or die('Error connecting to MySQL server.'.mysqli_error());
 // Create connection
-mysqli_set_charset($con, "utf8");
+mysqli_set_charset($dbc, "utf8");
 ?>
