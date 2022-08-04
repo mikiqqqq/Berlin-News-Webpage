df0188378fd4041c04d8a3c87e3af1afc0d66cfa
mikiqqqq <107814991+mikiqqqq@users.noreply.github.com>
2022-08-04 16:37:18 +0200

    Update home.php

--- home.php
+++ home.php
@@ -95,7 +95,7 @@
               echo '<div>';
                 echo '<a class="kultur-link" href="news-article.php?id='.$row['id'].'">';
                   echo '<img src="' . UPLPATH . $row['slika'] . '"
-                        alt="'. $row['slika'] .'"/>/>';
+                        alt="'. $row['slika'] .'"/>';
                   echo "<h4>" .$row['sazetak']. '</h4>';
                   echo '<h3>' .$row['naslov']. '</h3>';
                 echo '</a>';
